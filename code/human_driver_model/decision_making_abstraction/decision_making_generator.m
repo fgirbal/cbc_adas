e9c39af908dc151ec144dcff6462974a231463e1
Francisco Eiras <francisco.girbal.eiras@tecnico.ulisboa.pt>
2018-04-26 16:09:37 +0100

    Initial property testing
    Tested the safety property of crashing in different types of drivers
    (unsuccessful representation).

--- code/human_driver_model/decision_making_abstraction/decision_making_generator.m
+++ code/human_driver_model/decision_making_abstraction/decision_making_generator.m
@@ -16,9 +16,12 @@ ds = linspace(1,80,80);
 % Possible vehicle initial velocity
 vs = linspace(15,34,20);
 
-generated_table = zeros(3*length(ds)*length(vs), 7);
+generated_table = zeros(3*length(ds)*length(vs) + 3*length(ds), 7);
 display(sprintf('Generating table of %d entries', size(generated_table,1)))
 t1 = cputime;
+
+mean_dt = [0,2,4];
+std_dt = [0.6,0.3,1];
 
 % the model will have a parameter 'driver_type' which is equal to
 % 1 if aggressive, 2 if average or 3 if conservative
@@ -31,36 +34,51 @@ for driver_i = 1:3
 
             delta_crash = d/v;
 
-            x = [-3:.1:3];
-            norm = normpdf(x,0,1);
+            plC = 2*std_dt(driver_i)*normpdf(delta_crash,mean_dt(driver_i),std_dt(driver_i));
 
             idx = (driver_i - 1)*length(ds)*length(vs) + (d_i - 1)*length(vs) + v_i;
 
-            generated_table(idx,:) = [driver_i,1,d,v,delta_crash,0,1];
+            generated_table(idx,:) = [1,driver_i,d,v,round(delta_crash,2),round(plC,2),1-round(plC,2)];
         end
+    end
+end
+
+mean_dd = [10,40,70];
+std_dd = [20,7,20];
+
+for driver_i = 1:3
+    for d_i = 1:length(ds)
+        
+        d = ds(d_i);
+        
+        plC = 2*std_dd(driver_i)*normpdf(d,mean_dd(driver_i),std_dd(driver_i));
+        
+        idx = 3*length(ds)*length(vs) + (driver_i - 1)*length(ds) + d_i;
+
+        generated_table(idx,:) = [2,driver_i,d,-1,0,round(plC,2),1-round(plC,2)];
     end
 end
 
 display(sprintf('Generated in %.3f seconds', cputime - t1))
 
 % Display the table
-header = {'type','lane','d','v','delta_crash','P_lC','P_nlC'};
+header = {'lane','type','d','v','delta_crash','P_lC','P_nlC'};
 xForDisplay = [header; num2cell(generated_table)];
 disp(xForDisplay)
 
 % Save the table generated to a CSV file with a header
-% cHeader = header;
-% commaHeader = [cHeader;repmat({','},1,numel(cHeader))]; %insert commaas
-% commaHeader = commaHeader(:)';
-% textHeader = cell2mat(commaHeader); %cHeader in text with commas
-% textHeader = textHeader(1:end-1);
-% 
-% %write header to file
-% fid = fopen(sprintf('data/dm_table_%d_%d_%d_%d_%d_%d.csv', ds(1), ds(length(ds)),vs(1), vs(length(vs)), vi2s(1), vi2s(length(vi2s))),'w'); 
-% fprintf(fid,'%s\n',textHeader);
-% fclose(fid);
-% 
-% %write data to end of file
-% dlmwrite(sprintf('data/dm_table_%d_%d_%d_%d_%d_%d.csv', ds(1), ds(length(ds)),vs(1), vs(length(vs)), vi2s(1), vi2s(length(vi2s))),generated_table,'-append');
+cHeader = header;
+commaHeader = [cHeader;repmat({','},1,numel(cHeader))]; %insert commaas
+commaHeader = commaHeader(:)';
+textHeader = cell2mat(commaHeader); %cHeader in text with commas
+textHeader = textHeader(1:end-1);
+
+%write header to file
+fid = fopen('data/dm_table.csv','w'); 
+fprintf(fid,'%s\n',textHeader);
+fclose(fid);
+
+%write data to end of file
+dlmwrite('data/dm_table.csv',generated_table,'-append');
 
 %------------- END OF CODE --------------
--- code/human_driver_model/two_module_model/model_generator.py
+++ code/human_driver_model/two_module_model/model_generator.py
@@ -5,7 +5,7 @@
 # Author: Francisco Girbal Eiras, MSc Computer Science
 # University of Oxford, Department of Computer Science
 # Email: francisco.eiras@cs.ox.ac.uk
-# 24-Apr-2018; Last revision: 24-Apr-2018
+# 25-Apr-2018; Last revision: 26-Apr-2018
 
 import sys, csv
 
@@ -16,44 +16,73 @@ if len(sys.argv) < 3:
 f = open("two_component_model.pm", "w")
 
 v1 = "30";
-driver_type = "1"
+driver_type = "2"
+x1_0 = "15"
+v = "32"
 
 # Write the beginning of the file
 f.write("//Model automatically built using model_generator.py for v1 = %s and driver_type = %s (to alter this value, run the script again).\n\n"%(v1,driver_type))
 f.write("dtmc\n\n")
 f.write("const int length = 500; // road length\n")
 f.write("const int driver_type = 1; // 1 = aggressive, 2 = average, 3 = conservative drivers\n")
-f.write("const int max_time = 30; // maximum time of experiment\n")
-f.write("const int v1 = %s;\n"%v1)
-f.write("const int x1_0 = 5;\n\n")
-f.write("global actrState : [1..2] init 1; // active module: 1 = control (both cars), 2 = decision making + monitoring\n")
-f.write("global lC : bool init false; // lane changing occuring? \n")
+f.write("const int max_time = 30; // maximum time of experiment\n\n")
+f.write("// Other vehicle\n")
+f.write("const int v1 = %s; // do not alter this!\n"%v1)
+f.write("const int x1_0 = %s;\n\n"%x1_0)
+f.write("// Environment variables\n")
 f.write("global t : [0..max_time] init 0; // time \n")
 f.write("global crashed : bool init false; \n\n")
+f.write("// Vehicle controlled\n")
+f.write("global actrState : [1..2] init 1; // active module: 1 = control (both cars), 2 = decision making + monitoring\n")
+f.write("global lC : bool init false; // lane changing occuring? \n")
+f.write("global x : [0..length] init 0;\n")
+f.write("global v : [15..34] init %s;\n"%v)
+f.write("global lane : [1..2] init 1;\n\n")
 f.write("formula x1 = x1_0 + v1*t;\n")
-f.write("formula dist = x1>x?(x1 - x):(x - x1);\n\n")
+f.write("formula dist = x1>x?(x1 - x):(x - x1);\n")
+f.write("formula positiveDist = (x < length)?x > x1:true;\n\n")
 
 # Decision making + monitoring module
 f.write("module Decision_Making_Monitoring\n\n")
 f.write(" 	// If a crash occurs, then nothing else can happen\n")
 f.write("	[] actrState = 2 & crashed -> 1:(crashed' = true);\n\n")
 
+f.write(" 	// If we are in lane 2, but behind the other vehicle, don't try to pass\n")
+f.write("	[] actrState = 2 & !crashed & lane = 2 & positiveDist = false -> 1:(actrState' = 1);\n\n")
+
+# should we change from lane 1 to lane 2? it's based on delta_crash!
 with open(sys.argv[2]) as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
-		if row["type"] == driver_type:
-			line = "	[] actrState = 2 & !crashed & lane = 1 & dist = %s & v = %s -> %s:(actrState' = 1) & (lC' = true) + %s:(actrState' = 1) & (lC' = false);\n" % (row["d"],row["v"],row["P_lC"],row["P_nlC"])
+		if row["type"] == driver_type and row["lane"] == "1":
+			line = "	[] actrState = 2 & !crashed & lane = 1 & positiveDist = false & dist = %s & v = %s -> %s:(actrState' = 1) & (lC' = true) + %s:(actrState' = 1) & (lC' = false);\n" % (row["d"],row["v"],row["P_lC"],row["P_nlC"])
+			f.write(line)
+
+# should we go back to lane 1 from lane 2? it's based on the distance we are at!
+with open(sys.argv[2]) as csvfile:
+	reader = csv.DictReader(csvfile)
+	for row in reader:
+		if row["type"] == driver_type and row["lane"] == "2":
+			line = "	[] actrState = 2 & !crashed & lane = 2 & positiveDist = true & dist = %s -> %s:(actrState' = 1) & (lC' = true) + %s:(actrState' = 1) & (lC' = false);\n" % (row["d"],row["P_lC"],row["P_nlC"])
 			f.write(line)
 
 f.write("endmodule\n\n")
 
 # Control module
 f.write("module Control\n\n")
-f.write("	x : [0..length] init 0;\n")
-f.write("	v : [15..34] init 29;\n")
-f.write("	lane : [1..2] init 1;\n\n")
 
-f.write("	[] actrState = 1 & !crashed & !lC & lane = 1 & x <= length - v & t < max_time -> 1:(x' = x + v) & (t' = t + 1) & (actrState' = 2);\n\n")
+f.write(" 	// If we are in lane 1, and no lane change was decided, continue forward (which might result in crash)\n")
+f.write(" 	// The vehicle is behind the other driver (positiveDist = false, x < x1)\n")
+f.write("	[] actrState = 1 & !crashed & !lC & lane = 1 & x <= length - v & t < max_time & positiveDist = false & (x1 + v1 - x - v) >= 6 -> 1:(x' = x + v) & (t' = t + 1) & (actrState' = 2);\n")
+f.write("	[] actrState = 1 & !crashed & !lC & lane = 1 & x > length - v & t < max_time & positiveDist = false & (x1 + v1 - x - v) >= 6 -> 1:(x' = length) & (t' = t + 1) & (actrState' = 2);\n")
+f.write("	[] actrState = 1 & !crashed & !lC & lane = 1 & x <= length - v & t < max_time & positiveDist = false & (x1 + v1 - x - v) < 6 -> 1:(x' = x + v) & (t' = t + 1) & (crashed' = true) & (actrState' = 2);\n")
+f.write(" 	// The vehicle is in front of the other driver (positiveDist = true, x > x1)\n")
+f.write("	[] actrState = 1 & !crashed & !lC & lane = 1 & x <= length - v & t < max_time & positiveDist = true -> 1:(x' = x + v) & (t' = t + 1) & (actrState' = 2);\n")
+f.write("	[] actrState = 1 & !crashed & !lC & lane = 1 & x > length - v & t < max_time & positiveDist = true -> 1:(x' = length) & (t' = t + 1) & (actrState' = 2);\n")
+
+f.write(" 	// If we are in lane 2, and no lane change was decided, continue forward\n")
+f.write("	[] actrState = 1 & !crashed & !lC & lane = 2 & x <= length - v & t < max_time -> 1:(x' = x + v) & (t' = t + 1) & (actrState' = 2);\n")
+f.write("	[] actrState = 1 & !crashed & !lC & lane = 2 & x > length - v & t < max_time -> 1:(x' = length) & (t' = t + 1) & (actrState' = 2);\n\n")
 
 with open(sys.argv[1]) as csvfile:
 	reader = csv.DictReader(csvfile)
@@ -66,12 +95,12 @@ with open(sys.argv[1]) as csvfile:
 		if row["d"] != "40" and row["vi2"] == v1:
 			line = "	[] actrState = 1 & !crashed & lC & lane = %s & dist = %s & v = %s & x <= length - %s & t <= max_time - %s -> 1:(crashed' = %s) & (x' = x + %s) & (v' = %s) & (t' = t + %s) & (lane' = %s) & (actrState' = 2) & (lC' = false);\n" % (row["o_lane"], row["d"],row["vi1"],row["delta_x1"],row["delta_t"],acc_val,row["delta_x1"],row["vf1"],row["delta_t"],str(3 - int(row["o_lane"])))
 			f.write(line)
-			line = "	[] actrState = 1 & !crashed & lC & lane = %s & dist = %s & v = %s & x > length - %s & t <= max_time - %s -> 1:(crashed' = %s) & (x' = 500) & (v' = %s) & (t' = t + %s) & (lane' = %s) & (actrState' = 2) & (lC' = false);\n" % (row["o_lane"], row["d"],row["vi1"],row["delta_x1"],row["delta_t"],acc_val,row["vf1"],row["delta_t"],str(3 - int(row["o_lane"])))
+			line = "	[] actrState = 1 & !crashed & lC & lane = %s & dist = %s & v = %s & x > length - %s & t <= max_time - %s -> 1:(crashed' = %s) & (x' = length) & (v' = %s) & (t' = t + %s) & (lane' = %s) & (actrState' = 2) & (lC' = false);\n" % (row["o_lane"], row["d"],row["vi1"],row["delta_x1"],row["delta_t"],acc_val,row["vf1"],row["delta_t"],str(3 - int(row["o_lane"])))
 			f.write(line)
 		elif row["d"] == "40" and row["vi2"] == v1:
 			line = "	[] actrState = 1 & !crashed & lC & lane = %s & dist >= %s & v = %s & x <= length - %s & t <= max_time - %s -> 1:(crashed' = %s) & (x' = x + %s) & (v' = %s) & (t' = t + %s) & (lane' = %s) & (actrState' = 2) & (lC' = false);\n" % (row["o_lane"], row["d"],row["vi1"],row["delta_x1"],row["delta_t"],acc_val,row["delta_x1"],row["vf1"],row["delta_t"],str(3 - int(row["o_lane"])))
 			f.write(line)
-			line = "	[] actrState = 1 & !crashed & lC & lane = %s & dist >= %s & v = %s & x > length - %s & t <= max_time - %s -> 1:(crashed' = %s) & (x' = 500) & (v' = %s) & (t' = t + %s) & (lane' = %s) & (actrState' = 2) & (lC' = false);\n" % (row["o_lane"], row["d"],row["vi1"],row["delta_x1"],row["delta_t"],acc_val,row["vf1"],row["delta_t"],str(3 - int(row["o_lane"])))
+			line = "	[] actrState = 1 & !crashed & lC & lane = %s & dist >= %s & v = %s & x > length - %s & t <= max_time - %s -> 1:(crashed' = %s) & (x' = length) & (v' = %s) & (t' = t + %s) & (lane' = %s) & (actrState' = 2) & (lC' = false);\n" % (row["o_lane"], row["d"],row["vi1"],row["delta_x1"],row["delta_t"],acc_val,row["vf1"],row["delta_t"],str(3 - int(row["o_lane"])))
 			f.write(line)
 
 f.write("\nendmodule")
