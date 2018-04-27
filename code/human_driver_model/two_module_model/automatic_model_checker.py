ce4df5077fe28521386aef6f3843b752f70a78f8
Francisco Eiras <francisco.girbal.eiras@tecnico.ulisboa.pt>
2018-04-27 11:53:53 +0100

    Changes to the automatic model checking file
    Making it easier to test the three types of drivers and obtain the
    results desired.

--- code/human_driver_model/two_module_model/automatic_model_checker.py
+++ code/human_driver_model/two_module_model/automatic_model_checker.py
@@ -5,30 +5,40 @@
 # Author: Francisco Girbal Eiras, MSc Computer Science
 # University of Oxford, Department of Computer Science
 # Email: francisco.eiras@cs.ox.ac.uk
-# 26-Apr-2018; Last revision: 26-Apr-2018
+# 26-Apr-2018; Last revision: 27-Apr-2018
 
-import os, subprocess
+import sys, os, subprocess
 
-driver_type = 1
-v = 31
-v1 = 30
-x1_0 = 15
-filename = "gen_model_%s_%s_%s_%s"%(driver_type,v,v1,x1_0)
-r_filename = "results_%s_%s_%s_%s"%(driver_type,v,v1,x1_0)
+types = ['aggressive','average','cautious']
 
-# Construct the file
-print('Generating the model...')
-os.system('python3 model_generator.py model_tables/control_table.csv model_tables/dm_table.csv %s %s %s %s --filename results/%s > /dev/null'%(driver_type,v,v1,x1_0,filename))
+v = 20
+v1 = 15
+x1_0 = 30
 
-print('Building the model and performing model checking...')
-#os.system('prism results/%s.pm properties.pctl -exportresults results/%s.txt'%(filename, r_filename))
-subprocess.run("prism results/%s.pm properties.pctl -exportresults results/%s.txt"%(filename, r_filename), shell=True)
+for driver_type in range(1,4):
+	print('------ %s driver ------'%types[driver_type-1])
 
+	filename = "gen_model_%s_%s_%s_%s"%(driver_type,v,v1,x1_0)
+	r_filename = "results_%s_%s_%s_%s"%(driver_type,v,v1,x1_0)
 
-print('Obtaining the results...')
+	# Construct the file
+	print('Generating the model...')
+	os.system('python3 model_generator.py model_tables/control_table.csv model_tables/dm_table.csv %s %s %s %s --filename results/%s > /dev/null'%(driver_type,v,v1,x1_0,filename))
 
-print('Cleaning up...')
+	print('Building the model and performing model checking...')
+	subprocess.run("prism results/%s.pm properties.pctl -exportresults results/%s.txt &> /dev/null"%(filename, r_filename), shell=True)
 
-os.system('rm results/%s.pm'%filename)
+	print('Obtaining the results...')
+
+	f = open("results/%s.txt"%r_filename, "r")
+	f.readline();
+	probability = f.readline();
+	probability = float(probability[:-1])
+
+	print('The probability of crashing for an %s driver is: %f.'%(types[driver_type-1],probability))
+
+	print('Cleaning up...')
+
+	os.system('rm results/%s.pm'%filename)
 
 print('Done.')
